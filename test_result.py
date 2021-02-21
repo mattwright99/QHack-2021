ef9ccebd75f45da91ac60e5a9dd454d6665d4cfe
Matt Wright <16mpw1@queensu.ca>
2021-02-21 13:53:41 -0500

    Added array functionality

--- test_result.py
+++ test_result.py
@@ -1,11 +1,29 @@
 '''
 This script defines the helper function `test_result` which evaluates my quantum circuits' performace using 
-a given true value and tolerance.
+a given true value and relative tolerance.
 '''
+
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
 
 def test_result(pred, ans, tol):
     diff = abs(pred - ans) / ans
-    if diff < tol:
-        print('Success!!')
+    
+    if type(pred) == float:
+        if diff < tol:
+            print('Success!!')
+        else:
+            print(f'Failure, off by {diff}.')
+    
     else:
-        print(f'Failure, off by {diff}.')
+        # If the true result is 0.0 then check that the prediction is less than the tolerance
+        
+        inf_indices = ans == 0
+        diff[inf_indices] = pred[inf_indices]
+        
+        if all(diff < tol):
+            print('Success!!')
+        else:
+            print(f'Failure :(')

6e65b36daf7ae956de025b2fc394fd9d56227a5f
Matt Wright <16mpw1@queensu.ca>
2021-02-21 00:48:59 -0500

    Updated helper function description

--- test_result.py
+++ test_result.py
@@ -1,3 +1,8 @@
+'''
+This script defines the helper function `test_result` which evaluates my quantum circuits' performace using 
+a given true value and tolerance.
+'''
+
 def test_result(pred, ans, tol):
     diff = abs(pred - ans) / ans
     if diff < tol:
